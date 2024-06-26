33535f099f0a6026e4823cec5cf5c1a277633e85
nini98 <51435372+nini98@users.noreply.github.com>
2024-06-26 18:53:44 +0900

    measureExecutionTimeByPackage 메소드 비활성화

--- src/main/java/com/example/controlsystemsample/common/aspect/ExecutionTimeAspect.java
+++ src/main/java/com/example/controlsystemsample/common/aspect/ExecutionTimeAspect.java
@@ -10,14 +10,14 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class ExecutionTimeAspect {
-    @Around("execution(* com.example.controlsystemsample.service..*(..))")
-    public Object measureExecutionTimeByPackage(ProceedingJoinPoint joinPoint) throws Throwable {
-        long startTime = System.currentTimeMillis();
-        Object proceed = joinPoint.proceed();
-        long endTime = System.currentTimeMillis();
-        log.debug("[measureExecutionTimeByPackage] Execution time of " + joinPoint.getSignature() + " : " + (endTime - startTime) + " ms");
-        return proceed;
-    }
+//    @Around("execution(* com.example.controlsystemsample.service..*(..))")
+//    public Object measureExecutionTimeByPackage(ProceedingJoinPoint joinPoint) throws Throwable {
+//        long startTime = System.currentTimeMillis();
+//        Object proceed = joinPoint.proceed();
+//        long endTime = System.currentTimeMillis();
+//        log.debug("[measureExecutionTimeByPackage] Execution time of " + joinPoint.getSignature() + " : " + (endTime - startTime) + " ms");
+//        return proceed;
+//    }
 
     @Around("@annotation(com.example.controlsystemsample.common.annotation.MeasureExecutionTime)")
     public Object measureExecutionTimeByAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
